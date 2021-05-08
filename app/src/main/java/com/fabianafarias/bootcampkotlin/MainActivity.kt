9c06d7e830b78e302af38c1af48ed742fdc7898e
fabiana farias <farias.fabianam@gmail.com>
2021-05-08 17:04:19 -0300

    Criando Options Menu

--- app/src/main/java/com/fabianafarias/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/bootcampkotlin/MainActivity.kt
@@ -2,6 +2,12 @@ package com.fabianafarias.bootcampkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         bindView()
         updateList()
     }
+
 
     private fun bindView() {
         rvList.adapter = adapter
@@ -42,5 +49,27 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
