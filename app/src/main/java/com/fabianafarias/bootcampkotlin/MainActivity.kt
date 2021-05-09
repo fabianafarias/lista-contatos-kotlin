16d010dbbee54f391772312b1e54c6da174c4944
fabiana farias <farias.fabianam@gmail.com>
2021-05-09 12:42:38 -0300

    Armazenando e acessando dados com SharedPreferences e debug

--- app/src/main/java/com/fabianafarias/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/bootcampkotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fabianafarias.bootcampkotlin
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,11 +12,14 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fabianafarias.bootcampkotlin.DetailActivity.Companion.EXTRA_CONTACT
 import com.fabianafarias.bootcampkotlin.R.layout.drawer_menu
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity(), ClickItemContactListener{
     private val rvList: RecyclerView by lazy {
@@ -28,8 +33,33 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener{
         setContentView(R.layout.activity_main)
 
         initDrawer()
+        fetchListContact()
         bindView()
-        updateList()
+
+    }
+
+    private fun fetchListContact() {
+        val list = arrayListOf(
+            Contact(
+                "Fabiana Farias",
+                "(27) 99908-9974",
+                "img.png"
+            ),
+            Contact(
+                "Rodrigo Farias",
+                "(27) 98869-4621",
+                "img.png"
+            )
+        )
+        getInstanceSharedPreferences()?.edit {
+            val json = Gson().toJson(list)
+            putString("contacts", json)
+            commit()
+        }
+    }
+
+    private fun getInstanceSharedPreferences(): SharedPreferences? {
+        return getSharedPreferences("com.fabianafarias.bootcampkotlin.PREFERENCES", Context.MODE_PRIVATE)
     }
 
     private fun initDrawer() {
@@ -47,23 +77,18 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener{
     private fun bindView() {
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact> {
+        val list = getInstanceSharedPreferences()?.getString("contacts", "[]")
+        val turnsType = object  : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
     }
 
     private fun updateList(){
-        adapter.updateList(
-            arrayListOf(
-                Contact(
-                    "Fabiana Farias",
-                    "(27) 99908-9974",
-                    "img.png"
-                ),
-                Contact(
-                    "Rodrigo Farias",
-                    "(27) 98869-4621",
-                    "img.png"
-                )
-            )
-        )
+        val list = getListContacts()
+        adapter.updateList(list)
     }
 
     private fun showToast(message: String){
